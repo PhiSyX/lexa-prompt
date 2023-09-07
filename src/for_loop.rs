64fd49a9b4d1c822ad27475d146e806f675a65dd
Mike 'PhiSyX' S <738010+PhiSyX@users.noreply.github.com>
2023-09-07 23:13:26 +0200

    style: header license file

--- src/for_loop.rs
+++ src/for_loop.rs
@@ -1,10 +1,19 @@
-/*
- * @author Mike 'PhiSyX' S. (https://github.com/PhiSyX)
- *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at https://mozilla.org/MPL/2.0/.
- */
+// ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+// ┃                           __    _            ____  ______                 ┃
+// ┃                    ____  / /_  (_)______  __/ __ \/ ____/                 ┃
+// ┃                   / __ \/ __ \/ / ___/ / / / /_/ / /                      ┃
+// ┃                  / /_/ / / / / (__  ) /_/ / _, _/ /___                    ┃
+// ┃                 / .___/_/ /_/_/____/\__, /_/ |_|\____/                    ┃
+// ┃                /_/                 /____/                                 ┃
+// ┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┫
+// ┃ Copyright: (c) 2023, Mike 'PhiSyX' S. (https://github.com/PhiSyX)         ┃
+// ┃ SPDX-License-Identifier: MPL-2.0                                          ┃
+// ┃ ╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌ ┃
+// ┃                                                                           ┃
+// ┃  This Source Code Form is subject to the terms of the Mozilla Public      ┃
+// ┃  License, v. 2.0. If a copy of the MPL was not distributed with this      ┃
+// ┃  file, You can obtain one at https://mozilla.org/MPL/2.0/.                ┃
+// ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
 
 use std::{error, fmt, str};
 
